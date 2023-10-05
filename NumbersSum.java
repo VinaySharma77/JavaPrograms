6597e8a80901df97fb84917a2a5fc0610011e4af
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-10-05 04:50:49 -0700

    Add files via upload
    This program is a Java Program to calculate sum of two numbers.

---
+++ NumbersSum.java
@@ -0,0 +1,9 @@
+class NumbersSum{
+	public static void main(String args[]){
+		int num1 = 20, num2 = 10;
+		int sum = num1 + num2;
+		System.out.println("First Number is : " + num1);
+		System.out.println("Second Number is : " + num2);
+		System.out.println("Sum of two numbers is : " + sum);
+	}
+}
