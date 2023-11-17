d3b5471ebe4cbfc47ab89bbaa9b93ef148f42309
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-17 19:29:58 +0530

    Add files via upload
    This is a Java Program to perform Multiplication on Two Matrix.

---
+++ MatrixMultiplication.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class MatrixMultiplication{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows for the first matrix: ");
+        int rowsA = scanner.nextInt();
+        System.out.print("Enter the number of columns for the first matrix: ");
+        int colsA = scanner.nextInt();
+        System.out.print("Enter the number of rows for the second matrix: ");
+        int rowsB = scanner.nextInt();
+        System.out.print("Enter the number of columns for the second matrix: ");
+        int colsB = scanner.nextInt();
+        if (colsA != rowsB) {
+            System.out.println("Matrices cannot be multiplied. Exiting program.");
+            System.exit(0);
+        }
+        int[][] matrixA = new int[rowsA][colsA];
+        int[][] matrixB = new int[rowsB][colsB];
+        int[][] resultMatrix = new int[rowsA][colsB];
+        System.out.println("Enter elements of the first matrix:");
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsA; j++) {
+                matrixA[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println("Enter elements of the second matrix:");
+        for (int i = 0; i < rowsB; i++) {
+            for (int j = 0; j < colsB; j++) {
+                matrixB[i][j] = scanner.nextInt();
+            }
+        }
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                for (int k = 0; k < colsA; k++) {
+                    resultMatrix[i][j] += matrixA[i][k] * matrixB[k][j];
+                }
+            }
+        }
+        System.out.println("Resultant Matrix after multiplication:");
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                System.out.print(resultMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
