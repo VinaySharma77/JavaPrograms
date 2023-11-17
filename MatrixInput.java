57c7febb52eef943e01b23a68c48dda0338af680
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-17 19:04:53 +0530

    Add files via upload
    This is a Java Program to Take Input For Matrix and Print Matrix.

---
+++ MatrixInput.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class MatrixInput{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int columns = scanner.nextInt();
+        int[][] matrix = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print("Enter element for row " + (i + 1) + ", column " + (j + 1) + ": ");
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println("Matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
