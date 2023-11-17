bf855c63507c27f7cd7c0164812c0bfa2bf2f797
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-17 15:06:44 +0530

    Add files via upload
    This is a Java Program to Transpose Matrix.

---
+++ TransposeMatrix.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class TransposeMatrix{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int columns = scanner.nextInt();
+        int[][] matrix = new int[rows][columns];
+        System.out.println("Enter the matrix elements:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print("Enter element at position [" + (i + 1) + "][" + (j + 1) + "]: ");
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println("Original Matrix:");
+        displayMatrix(matrix);
+        int[][] transposeMatrix = transpose(matrix);
+        System.out.println("Transpose Matrix:");
+        displayMatrix(transposeMatrix);
+    }
+    private static int[][] transpose(int[][] matrix) {
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        int[][] transposeMatrix = new int[columns][rows];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                transposeMatrix[j][i] = matrix[i][j];
+            }
+        }
+        return transposeMatrix;
+    }
+    private static void displayMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
