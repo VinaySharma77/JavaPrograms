d9413c95ce5e5a0abdf5386e83df02a4fd903b5d
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-17 19:17:19 +0530

    Add files via upload
    This is a Java Program to perform Matrix Addition.

---
+++ MatrixAddition2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class MatrixAddition2{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int m = scanner.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int n = scanner.nextInt();
+        System.out.println("Enter the elements of the first matrix:");
+        int[][] matrix1 = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("Enter element at position (" + (i + 1) + ", " + (j + 1) + "): ");
+                matrix1[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println("Enter the elements of the second matrix:");
+        int[][] matrix2 = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("Enter element at position (" + (i + 1) + ", " + (j + 1) + "): ");
+                matrix2[i][j] = scanner.nextInt();
+            }
+        }
+        int[][] result = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+        System.out.println("The result matrix is:");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
