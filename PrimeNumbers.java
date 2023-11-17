075e7c8e7b3e21945e827a538ba62c8770cf497a
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-17 17:50:56 +0530

    Add files via upload
    This is a Java Program to Print Prime Numbers between 1 to 20.

---
+++ PrimeNumbers.java
@@ -0,0 +1,24 @@
+public class PrimeNumbers{
+    public static void main(String[] args) {
+        displayPrimeNumbers(1, 20);
+    }
+    public static void displayPrimeNumbers(int start, int end) {
+        System.out.println("Prime numbers between " + start + " and " + end + ":");
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        } 
+        return true;
+    }
+}
