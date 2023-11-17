5b8de097a103060d17c585bf4abb6153a3ad21b7
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-17 17:05:35 +0530

    Add files via upload
    This is a Java Program to Count Words in a Paragraph.

---
+++ CountWords.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class CountWords{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String inputString = scanner.nextLine();
+        int wordCount = countWords(inputString);
+        System.out.println("Word count: " + wordCount);
+    }
+    public static int countWords(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+        String[] words = input.split("\\s+");
+        return words.length;
+    }
+}
