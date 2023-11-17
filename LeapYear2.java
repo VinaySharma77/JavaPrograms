f3f6500b45586efcb8a97b77a16df1268af96be2
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-17 17:27:23 +0530

    Add files via upload
    This is a Java Program to Find Leap Year.

---
+++ LeapYear2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class LeapYear2{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+    private static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
