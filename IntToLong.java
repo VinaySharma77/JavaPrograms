110033a0cf253d871e9f0f27d8623d96c0834935
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-18 17:10:36 +0530

    Add files via upload
    This is a Java Program to Convert Int to Long.

---
+++ IntToLong.java
@@ -0,0 +1,8 @@
+public class IntToLong{
+    public static void main(String[] args) {
+        int intValue = 42;
+        long longValue = intValue;
+        System.out.println("Original int value: " + intValue);
+        System.out.println("Converted long value: " + longValue);
+    }
+}
