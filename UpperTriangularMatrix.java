9dcfffc9f5c4e5e675362c3be889b51c12d7d492
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-18 14:41:34 +0530

    Add files via upload
    This is a Java Program to Print Upper Triangular Matrix.

---
+++ UpperTriangularMatrix.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class UpperTriangularMatrix{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows and columns for the matrix: ");
+        int n = scanner.nextInt();
+        int[][] matrix = new int[n][n];
+        System.out.println("Enter the elements of the matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println("Upper Triangular Matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j >= i) {
+                    System.out.print(matrix[i][j] + " ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
