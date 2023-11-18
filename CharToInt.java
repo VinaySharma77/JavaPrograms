90531836729e0a892dba7d101a8645b764abc231
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-18 17:23:34 +0530

    Add files via upload
    This is a Java Program to Convert Char to Int.

---
+++ CharToInt.java
@@ -0,0 +1,8 @@
+public class CharToInt{
+    public static void main(String[] args) {
+        char myChar = 'A';
+        int intValue = (int) myChar;
+        System.out.println("Original char: " + myChar);
+        System.out.println("Converted int value: " + intValue);
+    }
+}
