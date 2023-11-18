75fe11465700b61bacd76b4252e766dbc248ba6c
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-18 17:33:51 +0530

    Add files via upload
    This is a Java Program to Convert Int to Char.

---
+++ IntToChar.java
@@ -0,0 +1,8 @@
+public class IntToChar{
+    public static void main(String[] args) {
+        int intValue = 65; // ASCII value for 'A'
+        char charValue = (char) intValue;
+        System.out.println("Original int value: " + intValue);
+        System.out.println("Converted char value: " + charValue);
+    }
+}
