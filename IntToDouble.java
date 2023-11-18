1af36fb8a0ec43baa31f1ed1bb445020ece3d415
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-18 17:17:30 +0530

    Add files via upload
    This is a Java Program to Convert Int to Double.

---
+++ IntToDouble.java
@@ -0,0 +1,8 @@
+public class IntToDouble{
+    public static void main(String[] args) {
+        int intValue = 42;
+        double doubleValue = intValue;
+        System.out.println("Original int value: " + intValue);
+        System.out.println("Converted double value: " + doubleValue);
+    }
+}
